83a436261cca1407814fe1a904be6b9db1a23077
childotg <child@ocdstudio.net>
2022-05-26 09:57:52 +0200

    devops pro release

---
+++ 220526 - DevOps Pro Europe @Vilnius/Containers/Consumer/ConsumerTask.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Messaging.ServiceBus;
+using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public class ConsumerTask
+    {
+
+        public async Task RunAsync()
+        {
+            var client = new ServiceBusClient(Constants.SBConnection)
+               .CreateReceiver(Constants.SBTodoQueue);
+
+            while (true)
+            {
+                var msg = await client.ReceiveMessageAsync(TimeSpan.FromSeconds(60));
+                var message = msg.Body.ToString();
+                Console.WriteLine($"Receiving --- {message}");
+                await client.CompleteMessageAsync(msg);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ 220526 - DevOps Pro Europe @Vilnius/Containers/Producer/ProducerTask.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using Common;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    internal class ProducerTask
+    {
+        public ProducerTask()
+        {
+        }
+
+        internal async Task RunAsync()
+        {
+            var client = new ServiceBusClient(Constants.SBConnection)
+                .CreateSender(Constants.SBTodoQueue);
+
+            while (true)
+            {
+                var message = $"Hello! It's {DateTime.UtcNow}";
+                Console.WriteLine($"Sending --- {message}");
+                await client.SendMessageAsync(new ServiceBusMessage(message));
+                await Task.Delay(1000);
+            }
+            
+        }
+    }
+}
